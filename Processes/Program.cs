388ee77cbe4b2ee0afa3a087c0a0a01ae6ddbaa1
Assir Abdukhalikov <asirushtomblack@gmail.com>
2023-05-10 19:40:28 +0600

    Process using examples

---
+++ Processes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Processes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (Process myProcess = new Process())
+                {
+                    myProcess.StartInfo.UseShellExecute = false;
+
+                    myProcess.StartInfo.FileName = "notepad.exe";
+                    myProcess.StartInfo.CreateNoWindow = true;
+                    myProcess.Start();
+
+                    myProcess.WaitForExit();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            //Console.ReadLine();
+        }
+    }
+}
